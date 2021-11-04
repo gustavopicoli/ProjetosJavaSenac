bc339526379fd2413b3ded008c3da80cbf07f783
Administrador <Administrador@CAML193011>
2021-11-04 20:34:38 -0300

    Media de alunos

---
+++ MediaAluno/src/mediaaluno/MediaAluno.java
@@ -0,0 +1,28 @@
+package mediaaluno;
+
+import java.util.Scanner;
+
+public class MediaAluno {
+
+    public static void main(String[] args) {
+       
+        Scanner entrada = new Scanner (System.in);
+        String aluno;
+        double nota1, nota2, nota3, media;
+        
+        System.out.println("Informe seu nome");
+        aluno = entrada.nextLine();
+        
+        System.out.println("Digite sua primeira nota:");
+        nota1 = entrada.nextDouble();
+        
+        System.out.println("Digite sua segunda nota");
+        nota2 = entrada.nextDouble();
+        
+        System.out.println("Digite sua terceira nota");
+        nota3 = entrada.nextDouble();
+        
+        
+    }
+    
+}
